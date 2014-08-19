fd67a9f2150c5313f6734dbad3f37d359e7a7155
Stephen Parker <shurakaisoft@gmail.com>
2014-08-19 21:57:07 +1000

    added some content

---
+++ content/bookshelf.js
@@ -0,0 +1,11 @@
+/**
+ * A bookshelf is an array of book objects
+ * Each book has a title and an array of chapters.
+ * The value of chapters is the number of sub-sections in each chapter
+ */
+module.exports = [
+	{
+		"title": "Secrets of the JavaScript Ninja",
+		"chapters": [ 5, 5, 4, 6, 7, 4, 7, 6, 4, 6, 5, 5, 7, 5, 4]
+	}
+];
